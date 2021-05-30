1fc7b1dd0f79ee8800ca0d3a912786e25fd63380
Jordy Kristianata <jordykristianata@gmail.com>
2021-05-30 18:56:28 +0700

    Fix Loader, Logo, and Contacts

--- js/loader/script.js
+++ js/loader/script.js
@@ -3,14 +3,14 @@ $('.post_loader').css('display', 'none');
 $('.post_loader').addClass('wow animated fadeIn');
 // $('nav').hide();
 // $('.container-fluid').hide();
-setTimeout(function(){
+
+setTimeout(function() {
 	$('body').addClass('loaded');
-	setTimeout(function(){
-		$('nav').show();
-		$('.container-fluid').delay(3000).show();
-		setTimeout(function(){
-			$('.post_loader').css('display', '');
-		},1000);
-	},1000);
-}, 1000);
-// });
+	setTimeout(function() {
+		$('nav').css('display','');
+		$('.container-fluid').css('display','');
+		$('.post_loader').css('display', '');
+	},500);
+	$('nav').css('visibility','');
+	$('.container-fluid').css('visibility','');
+},500);
